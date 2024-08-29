2ea79b243304d4266a5e14cd541e271fd7e52dba
yusuf-khaan <142320884+yusuf-khaan@users.noreply.github.com>
2024-08-29 18:58:58 +0530

    new commit

--- Doc/src/App.jsx
+++ Doc/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Backgrounds from './components/Background';
 import Foreground from './components/Foreground';
-import Card from './components/card';
+import Card from './components/Card';
 
 
 function App() {
